d9153e0d05d9bfd547af503f4041ff8ca56f8c9b
11gather11 <11gather11@gmail.com>
2025-12-09 16:01:12 +0900

    init

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,26 @@ import { defineConfig } from 'vite'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(async ({ mode }) => {
-  const plugins = [
-    tsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tanstackStart({
-      srcDirectory: 'src',
-    }),
-    viteReact(),
-  ]
+	const plugins = [
+		tsConfigPaths({
+			projects: ['./tsconfig.json'],
+		}),
+		tanstackStart({
+			srcDirectory: 'src',
+		}),
+		viteReact(),
+	]
 
-  // Cloudflare plugin only for production build
-  if (mode === 'production') {
-    const { cloudflare } = await import('@cloudflare/vite-plugin')
-    plugins.unshift(cloudflare({ viteEnvironment: { name: 'ssr' } }))
-  }
+	// Cloudflare plugin only for production build
+	if (mode === 'production') {
+		const { cloudflare } = await import('@cloudflare/vite-plugin')
+		plugins.unshift(cloudflare({ viteEnvironment: { name: 'ssr' } }))
+	}
 
-  return {
-    server: {
-      port: 3000,
-    },
-    plugins,
-  }
+	return {
+		server: {
+			port: 3000,
+		},
+		plugins,
+	}
 })
